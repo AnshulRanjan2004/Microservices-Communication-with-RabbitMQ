848d9b95fbde41cdc5e729ff149b30d7b4a9840e
Anshul Ranjan <ItsAnshulRanjan@gmail.com>
2024-03-24 16:31:45 +0530

    basic flask

--- producer/producer.py
+++ producer/producer.py
@@ -1,12 +1,30 @@
-from fastapi import FastAPI
+import requests
+import os
+from flask import Flask, jsonify
 
-app = FastAPI()
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000))
 
-@app.get("/health_check")
-async def health_check():
-    #we'll put get ,post requests here that will get send to rabbitmq to communicate with other services
-    return {"message": "health check service is up and running!"}
-
+@app.route("/")
+def home():
+    return "Hello, this is a Flask Microservice"
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8004)
+    app.run(debug=True, host="0.0.0.0", port=port)
+
+BASE_URL = "https://dummyjson.com"
+@app.route('/products', methods=['GET'])
+def get_products():
+    response = requests.get(f"{BASE_URL}/products")
+    if response.status_code != 200:
+        return jsonify({'error': response.json()['message']}), response.status_code
+    products = []
+    for product in response.json()['products']:
+        product_data = {
+            'id': product['id'],
+            'title': product['title'],
+            'brand': product['brand'],
+            'price': product['price'],
+            'description': product['description']
+        }
+        products.append(product_data)
+    return jsonify({'data': products}), 200 if products else 204
