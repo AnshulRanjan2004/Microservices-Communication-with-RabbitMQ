7f60864848c6b7c8239141bb7c451d125a0613e0
Ashwin Ragupathy <ashwin.ragupathy@gmail.com>
2024-04-19 09:59:36 +0530

    Change in Insertion.py

--- consumer_two/insertion.py
+++ consumer_two/insertion.py
@@ -3,6 +3,9 @@ import logging
 import pymongo
 import pika
 
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
 # Connect to MongoDB database
 client = pymongo.MongoClient("mongodb://mongodb:27017/")
 db = client["database"]
@@ -14,30 +17,37 @@ parameters = pika.ConnectionParameters(host='rabbitmq', port=5672, credentials=c
 connection = pika.BlockingConnection(parameters=parameters)
 channel = connection.channel()
 
-
 # Declare the "insert_record" queue
 channel.queue_declare(queue='insert_record', durable=True)
 
 # Define a callback function to handle incoming messages
 def callback(ch, method, properties, body):
-    # Parse incoming message
-    body = body.decode()
-    body = json.loads(body)
-    # message = json.loads(body)
-
-    record = {
-        "product_id": body['product_id'],
-        "name": body['name'],
-        "price": body['price'],
-        "seller_name": body['seller_name'],
-    }
-    collection.insert_one(record)
-
-    # Acknowledge the message
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    try:
+        # Parse incoming message
+        body = json.loads(body)
+        logging.info(f"Received message: {body}")
+        
+        # Check if all required fields are present
+        required_fields = ['product_id', 'name', 'price', 'seller_name']
+        if all(field in body for field in required_fields):
+            record = {
+                "product_id": body['product_id'],
+                "name": body['name'],
+                "price": body['price'],
+                "seller_name": body['seller_name'],
+            }
+            collection.insert_one(record)
+            logging.info("Record inserted successfully.")
+        else:
+            logging.error("Message is missing required fields.")
+        
+        # Acknowledge the message
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        logging.error(f"Error processing message: {e}")
 
 # Start consuming messages from the "insert_record" queue
 channel.basic_consume(queue='insert_record', on_message_callback=callback)
 
-print('Waiting for messages...')
+logging.info('Waiting for messages...')
 channel.start_consuming()
